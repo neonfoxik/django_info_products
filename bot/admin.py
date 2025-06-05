747f5e7b8305d8e7dd161ff3327ba8d7e04fc142
Ilia_m <neonfoxik1@gmail.com>
2025-06-05 12:31:12 +0300

    123

--- bot/admin.py
+++ bot/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import User, goods_category, goods
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('user_name',)  # Удалены недопустимые поля
-    search_fields = ('user_name',)
-    ordering = ('-telegram_id',)  # Изменено на существующее поле
+    list_display = ('user_name', 'telegram_id', 'is_admin', 'is_ai', 'screenshots_count', 'last_screenshot_date')
+    list_filter = ('is_admin', 'is_ai')
+    search_fields = ('user_name', 'telegram_id')
+    readonly_fields = ('telegram_id', 'chat_history', 'warranty_data')
+    ordering = ('-telegram_id',)
 
     def get_queryset(self, request):
         """Переопределяем метод для обработки ошибок при получении пользователей."""
@@ -16,11 +18,23 @@ class UserAdmin(admin.ModelAdmin):
 class GoodsCategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+    ordering = ('name',)
 
 class GoodsAdmin(admin.ModelAdmin):
-    list_display = ('name', 'parent_category')
-    list_filter = ('parent_category',)
-    search_fields = ('name',)
+    list_display = ('name', 'parent_category', 'is_returned', 'extended_warranty')
+    list_filter = ('parent_category', 'is_returned')
+    search_fields = ('name', 'instructions', 'FAQ', 'warranty')
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'parent_category', 'image')
+        }),
+        ('Описание', {
+            'fields': ('instructions', 'FAQ', 'warranty')
+        }),
+        ('Гарантия', {
+            'fields': ('extended_warranty', 'is_returned')
+        }),
+    )
 
 admin.site.register(User, UserAdmin)
 admin.site.register(goods_category, GoodsCategoryAdmin)
--- bot/models.py
+++ bot/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(models.Model):
     telegram_id = models.CharField(
         primary_key=True,
-        max_length=50
+        max_length=50,
+        db_index=True
     )
     user_name = models.CharField(
         max_length=35,
         verbose_name='Имя',
+        db_index=True
     )
     is_admin = models.BooleanField(
         default=False,
-        verbose_name='Является ли администратором'
+        verbose_name='Является ли администратором',
+        db_index=True
     )
     is_ai = models.BooleanField(
         default=False,
-        verbose_name='Используется ли AI'
+        verbose_name='Используется ли AI',
+        db_index=True
     )
     chat_history = models.JSONField(
         verbose_name='История переписки пользователя',
@@ -33,15 +38,18 @@ class User(models.Model):
     )
     screenshots_count = models.IntegerField(
         default=0,
-        verbose_name='Количество отправленных скриншотов за день'
+        verbose_name='Количество отправленных скриншотов за день',
+        validators=[MinValueValidator(0)]
     )
     last_screenshot_date = models.DateField(
         default=timezone.now,
-        verbose_name='Дата последней отправки скриншота'
+        verbose_name='Дата последней отправки скриншота',
+        db_index=True
     )
     messages_count = models.IntegerField(
         default=0,
-        verbose_name='Количество сообщений в текущем действии'
+        verbose_name='Количество сообщений в текущем действии',
+        validators=[MinValueValidator(0)]
     )
     last_message_id = models.CharField(
         max_length=50,
@@ -49,35 +57,52 @@ class User(models.Model):
         blank=True,
         verbose_name='ID последнего сообщения'
     )
+
     def __str__(self):
         return str(self.user_name)
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+        ordering = ['-telegram_id']
+        indexes = [
+            models.Index(fields=['user_name']),
+            models.Index(fields=['is_admin']),
+            models.Index(fields=['is_ai']),
+        ]
+
 
 class goods_category(models.Model):
     name = models.CharField(
         max_length=100,
-        verbose_name='Название категории'
+        verbose_name='Название категории',
+        unique=True,
+        db_index=True
     )
+
     def __str__(self):
         return str(self.name)
 
     class Meta:
         verbose_name = 'Категория товаров'
         verbose_name_plural = 'Категории товаров'
-    
-    
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+
+
 class goods(models.Model):
     parent_category = models.ForeignKey(
         goods_category,
         on_delete=models.CASCADE,
-        verbose_name='Родительская категория'
+        verbose_name='Родительская категория',
+        related_name='goods'
     )
     name = models.CharField(
         max_length=100,
-        verbose_name='Название товара'
+        verbose_name='Название товара',
+        db_index=True
     )
     image = models.ImageField(
         upload_to='products/',
@@ -96,16 +121,24 @@ class goods(models.Model):
     )
     extended_warranty = models.FloatField(
         verbose_name='Срок расширенной гарантии (в годах)',
-        default=1.0
+        default=1.0,
+        validators=[MinValueValidator(0.1), MaxValueValidator(10.0)]
     )
     is_returned = models.BooleanField(
         default=False,
-        verbose_name='Товар возвращен'
+        verbose_name='Товар возвращен',
+        db_index=True
     )
 
     def __str__(self):
         return str(self.name)
 
     class Meta:
-        verbose_name = 'Название товара'
-        verbose_name_plural = 'Названия товаров'
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+            models.Index(fields=['is_returned']),
+            models.Index(fields=['parent_category']),
+        ]
